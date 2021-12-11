be86f7166633f64252718c9214a9b665a0c40040
Matteo Gregoricchio <matteo.gregoricchio@libero.it>
2021-12-11 22:31:01 +0100

    chore(wip): not-even-draft

--- Twise/src/Converters/ITwiseTypeConverter.cs
+++ Twise/src/Converters/ITwiseTypeConverter.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Twise.src.Converters
 {
-    interface ITwiseTypeConverter
-    {
-    }
+  interface ITwiseTypeConverter
+  {
+  }
 }
--- Twise/src/Deserializers/ITwiseDeserializer.cs
+++ Twise/src/Deserializers/ITwiseDeserializer.cs
@@ -1,21 +1,21 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Twise.src.Converters;
 using Twise.src.Types;
 
 namespace Twise.src.Deserializers
 {
-    interface ITwiseDeserializer
-    {
-        IEnumerable<ITwiseProperty> TwiseValues { get; set; }
-        dynamic Content { get; set; }
-        ITwiseTypeConverter GetConverter(Type twiseType);
-        // on each iteration, dynamic property will be checked for existence
-        // it will receive next twiseProperty, which will inform how the item should be casted
-        // at the end, it will receive T
-        // if the expected T isn't the one out of convert, throw
-        T SafeConvert<T>(dynamic currentItemContentIteration); // move in ITwiseTypeConverter
+  interface ITwiseDeserializer
+  {
+    IEnumerable<ITwiseProperty> TwiseValues { get; set; }
+
+    ITwiseTypeConverter GetConverter(Type twiseType);
+    // on each iteration, dynamic property will be checked for existence
+    // it will receive next twiseProperty, which will inform how the item should be casted
+    // at the end, it will receive T
+    // if the expected T isn't the one out of convert, throw
+    T SafeConvert<T>(dynamic currentItemContentIteration); // move in ITwiseTypeConverter
 
 
-    }
+  }
 }
